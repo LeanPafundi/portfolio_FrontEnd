e8449a17a66641e1f6f321b7a4c854c17b0e9427
Leandro Pafundi <leandro.pafundi@gmail.com>
2022-07-11 12:52:59 -0300

    se agrega javascript

---
+++ ejercicios/javascript/switch.js
@@ -0,0 +1,20 @@
+var frase = "";
+
+var musico = "Calamaro";
+
+switch(musico){
+    case "Calamaro":
+        frase = "Quiero elegir del mapa un lugar sin nombre a donde ir, será el lugar donde viva lo que quede por vivir";
+        break;
+
+    case "Charly":
+        frase = "No elegí este mundo pero aprendí a querer";
+        break;
+
+    case "L-Gante":
+        frase ="Esquivando los controle";
+    break;
+}
+
+alert(frase);
+
